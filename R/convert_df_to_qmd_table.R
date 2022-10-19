77a567698f07d1a50eec9c157a22f0644eb532ef
szeller42 <s.zeller@posteo.net>
2022-10-19 11:17:53 +0200

    ✨ add possibility to align columns in convert_to_qmd_table

--- R/convert_df_to_qmd_table.R
+++ R/convert_df_to_qmd_table.R
@@ -8,8 +8,12 @@
 #' @param df The data frame that should be converted
 #' @param column_names The column names that should be printed.
 #' Defaults to \code{names(df)}.
+#' @param align An optional character vector to define how the columns should be aligned.
+#' Each element can be either "l" (left), "r" (right), or "c" (center).
+#' The position of each element corresponds with the position of each column in \code{column_names}.
 #'
 #' @importFrom dplyr mutate
+#' @import assertthat
 #'
 #' @export
 #' @return A qmd table.
@@ -17,9 +21,17 @@
 #' @examples
 #' convert_to_qmd_table(iris[1:10,])
 
+
 convert_to_qmd_table <-
   function(df,
-           column_names = names(df)[-ncol(df)]){
+           column_names = NA,
+           align = NA){
+
+    if (missing(column_names)) {column_names <- names(df)}
+
+    assert_that(is.character(align) | missing(align))
+    if(!missing(align)) {assert_that(length(column_names) == length(align),
+                                     msg = "Please adjust the length of align to the number of columns.")}
 
     df <- df |>
       mutate(paragraph = "\n")
@@ -29,10 +41,19 @@ convert_to_qmd_table <-
       text <- paste0(c(text, df[row, ]))
     }
 
+    if (all(align %in% c("c", "r", "l"))) {
+      paste0(c("", column_names, "\n",
+               align_columns(align),
+               text),
+             collapse = "|") |>
+        cat()
 
-    paste0(c("", column_names, "\n",
-             rep("-----", length(column_names)),
-             text),
-           collapse = "|") |>
-      cat()
+    } else {
+      paste0(c("", column_names, "\n",
+               rep("-----", length(column_names)),
+               text),
+             collapse = "|") |>
+        cat()
+    }
+
   }
