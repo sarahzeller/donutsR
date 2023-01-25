78ab9456d00cc29e37302f4457563bf47fec69c2
szeller42 <s.zeller@posteo.net>
2023-01-25 16:33:20 +0100

    🐛 output p-values as numeric

--- R/tidy_custom.fixest.R
+++ R/tidy_custom.fixest.R
@@ -6,23 +6,21 @@
 #' @keywords internal
 #' @export
 
-tidy_custom.fixest <- function(x, ...) {
-  if (("donut_list" %in% class(x) | "donut_model" %in% class(x) ) &
+tidy_custom.fixest <- function (x, ...)
+{
+  if (("donut_list" %in% class(x) | "donut_model" %in% class(x)) &
       is.null(x[["bootstrap_dist"]]) == FALSE) {
-    pval <- coeftable(x)[, 4] |> format(digits = 4)
+    pval <- coeftable(x)[, 4] |> round(digits = 4)
     pval[["dist"]] <- ifelse(is.null(x[["bootstrap_dist"]]),
-                             pval[["dist"]],
-                             x[["bootstrap_dist"]][["p_val"]])
+                             pval[["dist"]], x[["bootstrap_dist"]][["p_val"]])
     s <- coeftable(x)[, 2]
     s[["dist"]] <- ifelse(is.null(x[["bootstrap_dist"]]),
-                          s[["dist"]],
-                          NA_integer_)
-    # figure out variable labels
-    labels <- x$call$data |> var_label(unlist = TRUE)
-
+                          s[["dist"]], NA_integer_)
+    labels <- var_label(x$call$data, unlist = TRUE)
     out <- data.frame(term = labels[names(pval)],
                       p.value = pval,
                       std.error = s)
     return(out)
   }
 }
+
