1de2c458abc472e429ff2f88badfd0c53bf9ae2a
szeller42 <s.zeller@posteo.net>
2023-01-25 14:55:00 +0100

    ✨ set tidy_custom names to var labels

--- R/tidy_custom.fixest.R
+++ R/tidy_custom.fixest.R
@@ -1,25 +1,27 @@
 #' Helper function for modelsummary
 #'
 #' @importFrom fixest coeftable
+#' @importFrom labelled var_label
 #'
 #' @keywords internal
 #' @export
 
 tidy_custom.fixest <- function(x, ...) {
   if ("donut_list" %in% class(x) | "donut_model" %in% class(x)) {
+    pval <- coeftable(x)[, 4] |> format(digits = 4)
+    pval[["dist"]] <- ifelse(is.null(x[["bootstrap_dist"]]),
+                             pval[["dist"]],
+                             x[["bootstrap_dist"]][["p_val"]])
+    s <- coeftable(x)[, 2]
+    s[["dist"]] <- ifelse(is.null(x[["bootstrap_dist"]]),
+                          s[["dist"]],
+                          NA_integer_)
+    # figure out variable labels
+    labels <- x$call$data |> var_label(unlist = TRUE)
 
-  pval <- coeftable(x)[,4] |> format(digits = 4)
-  pval[["dist"]] <- ifelse(is.null(x[["bootstrap_dist"]]),
-                           pval[["dist"]],
-                           x[["bootstrap_dist"]][["p_val"]])
-  s <- coeftable(x)[,2]
-  s[["dist"]] <- ifelse(is.null(x[["bootstrap_dist"]]),
-                        s[["dist"]],
-                        NA_integer_)
-  out <- data.frame(
-    term = names(pval),
-    p.value = pval,
-    std.error = s)
-  return(out)
+    out <- data.frame(term = labels[names(pval)],
+                      p.value = pval,
+                      std.error = s)
+    return(out)
   }
 }
