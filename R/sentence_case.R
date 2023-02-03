6e55c7bd97e1a95166fc7c7a86dd0aaa25e94947
szeller42 <s.zeller@posteo.net>
2023-02-03 14:59:18 +0100

    ✨ add function to sentence-case characters

---
+++ R/sentence_case.R
@@ -0,0 +1,14 @@
+#' Helper function for model_summary
+#'
+#' Turns character vectors to sentence case, and retains the names.
+#'
+#' @keywords internal
+#' @export
+
+sentence_case <- function(x) {
+  x_labels <- names(x)
+  new_x <- paste0(substr(x, 1, 1) |> toupper(),
+                  substr(x, 2, nchar(x)) |> tolower())
+  names(new_x) <- x_labels
+  return(new_x)
+}
